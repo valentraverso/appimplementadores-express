bdb1ff3d446e968353c277733dfad808e0c19b83
Valentino Traverso <vt20032003@live.com.ar>
2023-06-03 21:05:43 +0200

    feat: error logger

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import cors from 'cors';
+import morgan from 'morgan';
+import fs from 'fs';
 import {
     DB_URI,
     PORT
@@ -6,11 +9,16 @@ import {
 import connectMongo from "./utils/db/connectMongo";
 import { jokesRoute } from "./routes";
 import signToken from "./utils/auth/signToken";
+import path from "path";
 
 const app = express();
 
 // Middlewars
+app.use(cors());
 app.use(express.json());
+app.use(morgan('tiny', {
+    stream: fs.createWriteStream(path.join(__dirname, 'error.log'), { flags: 'a' })
+}))
 
 // Connect to DB and run server
 connectMongo(app, DB_URI, PORT)
